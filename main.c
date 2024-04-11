0dcf58300dd37de76188184d6b52a43c4b282f3f
unknown <lraulino26@gmail.com>
2024-04-11 12:55:55 -0300

    v2 criação do menu e sistema de prioridade

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*definindo a variÃ¡vel de tamanho mÃ¡ximo*/
+/*definindo a variável de tamanho máximo*/
 #define MAX_SIZE 100
 #define TAM_MAX 10
 
@@ -24,29 +24,23 @@ struct Fila {
   int elementos[MAX_SIZE];
 };
 
-void inicializar(struct LstEstc* lista) {
-  lista->tamanho2 = 0;
-};
+void inicializar(struct LstEstc *lista) { lista->tamanho2 = 0; };
 
-/*criando a funÃ§Ã£o para inicializar a fila*/
-void inicializarFila(struct Fila* fila) {
+/*criando a função para inicializar a fila*/
+void inicializarFila(struct Fila *fila) {
   fila->inicio = 0;
   fila->fim = -1;
   fila->tamanho = 0;
 }
 
-/*funÃ§Ã£o para verificar se a fila estÃ¡ vazia*/
-int estaVazia(struct Fila* fila) {
-  return fila->tamanho == 0;
-}
+/*função para verificar se a fila está vazia*/
+int estaVazia(struct Fila *fila) { return fila->tamanho == 0; }
 
-/*funÃ§Ã£o para verificar se a fila estÃ¡ cheia*/
-int estaCheia(struct Fila* fila) {
-  return fila->tamanho == MAX_SIZE;
-}
+/*função para verificar se a fila está cheia*/
+int estaCheia(struct Fila *fila) { return fila->tamanho == MAX_SIZE; }
 
-/*funÃ§Ã£o para inserir um pedido*/
-void inserirPedido(struct Fila* fila, int valor) {
+/*função para inserir um pedido*/
+void inserirPedido(struct Fila *fila, int valor) {
   if (!estaCheia(fila)) {
     fila->fim = (fila->fim + 1) % MAX_SIZE;
     fila->elementos[fila->fim] = valor;
@@ -56,7 +50,7 @@ void inserirPedido(struct Fila* fila, int valor) {
   }
 }
 
-int dequeue(struct Fila* fila) {
+int dequeue(struct Fila *fila) {
   if (!estaVazia(fila)) {
     int valor = fila->elementos[fila->inicio];
     fila->inicio = (fila->inicio + 1) % MAX_SIZE;
@@ -68,7 +62,7 @@ int dequeue(struct Fila* fila) {
   }
 }
 
-int peek(struct Fila* fila) {
+int peek(struct Fila *fila) {
   if (!estaVazia(fila)) {
     return fila->elementos[fila->inicio];
   } else {
@@ -77,11 +71,11 @@ int peek(struct Fila* fila) {
   }
 }
 
-/*funÃ§Ã£o para imprimir a fila*/
+/*função para imprimir a fila*/
 
-int todosOsElementosDaFila(struct Fila* fila) {
+int todosOsElementosDaFila(struct Fila *fila) {
   if (!estaVazia(fila)) {
-    for(int i = 0; i < (id - 1); i++) {
+    for (int i = 0; i < (id - 1); i++) {
       printf("%i\n", fila->elementos[i]);
     }
   } else {
@@ -90,38 +84,41 @@ int todosOsElementosDaFila(struct Fila* fila) {
   }
 }
 
-/*funÃ§Ã£o para inserir pedido(lista) na fila de pedidos*/
+/*função para inserir pedido(lista) na fila de pedidos*/
 
-void inserir(struct LstEstc* lista, struct Fila* fila, const int* idPedido, const char* itemPedido, const char* nomeCliente) { 
-  if (lista->tamanho2 < TAM_MAX) { 
-    // Se a lista nÃ£o atingiu tamanho mÃ¡ximo
+void inserir(struct LstEstc *lista, struct Fila *fila, const int *idPedido,
+             const char *itemPedido, const char *nomeCliente) {
+  if (lista->tamanho2 < TAM_MAX) {
+    // Se a lista não atingiu tamanho máximo
     // pode inserir
 
-    // O valor do campo tamanho da lista Ã©
-    // equivalente Ã  prÃ³xima posiÃ§Ã£o livre do vetor
+    // O valor do campo tamanho da lista é
+    // equivalente à próxima posição livre do vetor
     lista->pedidosSimples[lista->tamanho2].idPedido = *idPedido;
     strcpy(lista->pedidosSimples[lista->tamanho2].itemPedido, itemPedido);
     strcpy(lista->pedidosSimples[lista->tamanho2].nomeCliente, nomeCliente);
-    // Incrementar o campo tamanho da lista 
-    lista->tamanho2++; 
+    // Incrementar o campo tamanho da lista
+    lista->tamanho2++;
     fila->fim = (fila->fim + 1) % MAX_SIZE;
     fila->elementos[fila->fim] = id;
     fila->tamanho++;
-  } else { 
-    // Se a lista jÃ¡ atingiu seu limite
-    printf("Lista cheia. ImpossÃ­vel adicionar\n");
+  } else {
+    // Se a lista já atingiu seu limite
+    printf("Lista cheia. Impossível adicionar\n");
   }
 }
 
-void exibir(struct LstEstc* lista) { 
-  printf("Lista de Pedidos:\n"); 
-  // Percorre do primeiro elemento atÃ© o Ãºltimo
-  // que Ã© aquele apontado pelo campo â€˜tamanhoâ€™
-  for (int i=0; i < lista->tamanho2; i++) { 
-    printf("idPedido: %i, Item: %s, Nome Cliente: %s\n", 
-      lista->pedidosSimples[i].idPedido, lista->pedidosSimples[i].itemPedido, lista->pedidosSimples[i].nomeCliente); 
-  } 
-} 
+void exibir(struct LstEstc *lista, char *TipoLista) {
+  printf("Lista de Pedidos %s :\n", TipoLista);
+  // Percorre do primeiro elemento até o último
+  // que é aquele apontado pelo campo ‘tamanho’
+  for (int i = 0; i < lista->tamanho2; i++) {
+    printf("idPedido: %i, Item: %s, Nome Cliente: %s\n",
+           lista->pedidosSimples[i].idPedido,
+           lista->pedidosSimples[i].itemPedido,
+           lista->pedidosSimples[i].nomeCliente);
+  }
+}
 
 int main() {
 
@@ -131,21 +128,24 @@ int main() {
   char nomeCliente[50];
 
   /*Inicia a fila de pedidos*/
-  
+
   struct Fila minhaFila;
   inicializarFila(&minhaFila);
 
   /*Inicia a lista de pedidos simples*/
-  
+
   struct LstEstc pedidosSimples1;
   inicializar(&pedidosSimples1);
 
-  /*inserir(&pedidosSimples1, &minhaFila, &id, "CafÃ©");
-  inserir(&pedidosSimples1, &minhaFila, &id, "CafÃ© com leite");
-  inserir(&pedidosSimples1, &minhaFila, &id, "Cookie");
-  inserir(&pedidosSimples1, &minhaFila, &id, "PÃ£o de queijo");*/
+  struct LstEstc pedidosComplexos1;
+  inicializar(&pedidosComplexos1);
 
-  while( condicao != 3 ) {
+  /*inserir(&pedidosSimples1, &minhaFila, &id, "Café");
+  inserir(&pedidosSimples1, &minhaFila, &id, "Café com leite");
+  inserir(&pedidosSimples1, &minhaFila, &id, "Cookie");
+  inserir(&pedidosSimples1, &minhaFila, &id, "Pão de queijo");*/
+
+  while (condicao != 3) {
     printf("|--------------------------|\n");
     printf("|  MENU CAFETERIA FUMANTE  |\n");
     printf("|--------------------------|\n");
@@ -156,45 +156,49 @@ int main() {
     printf("|3 - SAIR DO SISTEMA       |\n");
     printf("|--------------------------|\n");
     scanf("%i", &condicao);
-    if(condicao == 1 ) {
+    if (condicao == 1) {
       printf("\n");
       printf("|Digite o nome do cliente  \n");
       scanf("%s", nomeCliente);
       printf("|Digite a quantidade de itens no pedido  \n");
       scanf("%i", &quantidadeItensPedido);
-      for(int p = 0; p < quantidadeItensPedido; p++) {
+      for (int p = 0; p < quantidadeItensPedido; p++) {
         printf("|Digite o nome do produto  \n");
         scanf("%s", produto);
-        inserir(&pedidosSimples1, &minhaFila, &id, produto, nomeCliente);
+        if(quantidadeItensPedido <= 3) {
+          inserir(&pedidosSimples1, &minhaFila, &id, produto, nomeCliente);
+        } else {
+          inserir(&pedidosComplexos1, &minhaFila, &id, produto, nomeCliente);
+        }
       }
       printf("|Pedido adicionado a fila  \n");
       printf("\n");
       id++;
     }
-    if(condicao == 2) {
+    if (condicao == 2) {
       printf("\n");
-      exibir(&pedidosSimples1);
+      exibir(&pedidosSimples1, "Simples");
       printf("\n");
+      exibir(&pedidosComplexos1, "Complexos");
     }
   }
 
   /*cria os pedidos
 
-  printf("Elemento no inÃ­cio da fila: %d\n", 
+  printf("Elemento no início da fila: %d\n",
     peek(&minhaFila));
 
   printf("Removendo elemento: %d\n", dequeue(&minhaFila));
-  printf("Elemento no inÃ­cio da fila apÃ³s remoÃ§Ã£o: %d\n", 
+  printf("Elemento no início da fila após remoção: %d\n",
     peek(&minhaFila));
 
   printf("Todos os elementos da fila:\n");
   todosOsElementosDaFila(&minhaFila);
     */
-  
+
   // Exibir lista
   /*exibir(&pedidosSimples1);*/
-  
-  /*--------*/
 
   return 0;
 }
+
